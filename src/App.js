efa5b08f25837cbe019b50af1a4864f466c41ebb
Girish Nandan <jngnandan@gmail.com>
2023-03-17 01:36:45 +0000

    header added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
+import Header from './components/Header';
 import { BrowserRouter, Routers, Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+        <Header/>
       <Routes>
         <Route exact path='/' element={<Home/>}/>
         <Route exact path='/about' element={<About/>}/>
